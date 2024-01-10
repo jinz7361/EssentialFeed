fcb958037a02025768429c6178100ee7379d9b5f
jinz7361 <jinz7361@gmail.com>
2024-01-09 20:57:56 -0500

    Move ErrorView to separate file

---
+++ EssentialFeediOS/Feed UI/Views/ErrorView.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorView.swift
+//  EssentialFeediOS
+//
+//  Created by Jin Zhang on 1/9/24.
+//
+
+import UIKit
+
+public final class ErrorView: UIView {
+    public var message: String?
+}

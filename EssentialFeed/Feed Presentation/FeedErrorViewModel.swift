5cf4ad053b30e5578ac350ee289461f4d6df36e3
jinz7361 <jinz7361@gmail.com>
2024-01-09 22:38:46 -0500

    Move FeedErrorViewModel to separate file

---
+++ EssentialFeed/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  FeedErrorViewModel.swift
+//  EssentialFeed
+//
+//  Created by Jin Zhang on 1/9/24.
+//
+
+public struct FeedErrorViewModel {
+    public let message: String?
+    
+    static var noError: FeedErrorViewModel {
+        return FeedErrorViewModel(message: nil)
+    }
+    
+    static func error(message: String) -> FeedErrorViewModel {
+        return FeedErrorViewModel(message: message)
+    }
+}

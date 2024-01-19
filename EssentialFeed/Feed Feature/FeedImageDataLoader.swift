de44e847ead96bf65d7769a37e46692b3d669f63
jinz7361 <jinz7361@gmail.com>
2024-01-19 14:23:18 -0500

    Move FeedImageDataLoader to cross-platform EssentialFeed framework

--- EssentialFeed/Feed Feature/FeedImageDataLoader.swift
+++ EssentialFeed/Feed Feature/FeedImageDataLoader.swift
@@ -1,8 +1,8 @@
 //
 //  FeedImageDataLoader.swift
-//  EssentialFeediOS
+//  EssentialFeed
 //
-//  Created by Jin Zhang on 12/28/23.
+//  Created by Jin Zhang on 1/19/24.
 //
 
 import Foundation
@@ -13,5 +13,6 @@ public protocol FeedImageDataLoaderTask {
 
 public protocol FeedImageDataLoader {
     typealias Result = Swift.Result<Data, Error>
+    
     func loadImageData(from url: URL, completion: @escaping (Result) -> Void) -> FeedImageDataLoaderTask
 }

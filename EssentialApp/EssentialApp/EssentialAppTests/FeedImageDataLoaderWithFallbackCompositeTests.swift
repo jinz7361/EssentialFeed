415c225261c7414fba933dfb7afce59f9677e110
jinz7361 <jinz7361@gmail.com>
2024-02-04 17:18:45 -0500

    FeedImageDataLoaderWithFallbackComposite.init does not load image data

---
+++ EssentialApp/EssentialApp/EssentialAppTests/FeedImageDataLoaderWithFallbackCompositeTests.swift
@@ -0,0 +1,57 @@
+//
+//  FeedImageDataLoaderWithFallbackCompositeTests.swift
+//  EssentialAppTests
+//
+//  Created by Jin Zhang on 2/4/24.
+//
+
+import XCTest
+import EssentialFeed
+
+class FeedImageDataLoaderWithFallbackComposite: FeedImageDataLoader {
+    private class Task: FeedImageDataLoaderTask {
+        func cancel() {
+            
+        }
+    }
+    
+    init(primary: FeedImageDataLoader, fallback: FeedImageDataLoader) {
+        
+    }
+    
+    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> EssentialFeed.FeedImageDataLoaderTask {
+        return Task()
+    }
+    
+}
+
+class FeedImageDataLoaderWithFallbackCompositeTests: XCTestCase {
+    func test_init_doesNotLoadImageData() {
+        let primaryLoader = LoaderSpy()
+        let fallbackLoader = LoaderSpy()
+        _ = FeedImageDataLoaderWithFallbackComposite(primary: primaryLoader, fallback: fallbackLoader)
+        
+        XCTAssertTrue(primaryLoader.loadedURLs.isEmpty, "Expected no loadedURL in the primart loader")
+        XCTAssertTrue(fallbackLoader.loadedURLs.isEmpty, "Expected no loadedURL in the fallback loader")
+    }
+    
+    // MARK: - Helpers
+    
+    private class LoaderSpy: FeedImageDataLoader {
+        private let messages = [(url: URL, completion: (FeedImageDataLoader.Result) -> Void)]()
+        
+        var loadedURLs: [URL] {
+            return messages.map { $0.url }
+        }
+        
+        private struct Task: FeedImageDataLoaderTask {
+            func cancel() {
+                
+            }
+        }
+        
+        func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+            return Task()
+        }
+    }
+}

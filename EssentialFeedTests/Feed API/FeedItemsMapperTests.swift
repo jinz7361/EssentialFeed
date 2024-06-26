be8c9a62b4a646cc24002ae75297baf44c7b8b05
jinz7361 <jinz7361@gmail.com>
2024-06-26 19:14:27 -0400

    Remove unused helper

--- EssentialFeedTests/Feed API/FeedItemsMapperTests.swift
+++ EssentialFeedTests/Feed API/FeedItemsMapperTests.swift
@@ -58,11 +58,6 @@ class FeedItemsMapperTests: XCTestCase {
     }
     
     // MARK: - Helpers
-    
-    private func failure(_ error: RemoteFeedLoader.Error) -> RemoteFeedLoader.Result {
-        return .failure(error)
-    }
-    
     private func makeItem(id: UUID, description: String? = nil, location: String? = nil, imageURL: URL) -> (model: FeedImage, json: [String: Any]) {
         let item = FeedImage(id: id, description: description, location: location, url: imageURL)
         
@@ -74,10 +69,5 @@ class FeedItemsMapperTests: XCTestCase {
         ].compactMapValues { $0 }
         
         return (item, json)
-    }
-    
-    private func makeItemsJSON(_ items: [[String: Any]]) -> Data {
-        let json = ["items": items]
-        return try! JSONSerialization.data(withJSONObject: json)
     }
 }

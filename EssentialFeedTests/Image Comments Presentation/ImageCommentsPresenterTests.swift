36140d6efafeb0343c036936955cded92fcde392
jinz7361 <jinz7361@gmail.com>
2024-07-16 23:35:33 -0400

    Add image comments title

---
+++ EssentialFeedTests/Image Comments Presentation/ImageCommentsPresenterTests.swift
@@ -0,0 +1,26 @@
+//
+//  ImageCommentsPresenterTests.swift
+//  EssentialFeedTests
+//
+//  Created by Jin Zhang on 7/16/24.
+//
+
+import XCTest
+import EssentialFeed
+
+class ImageCommentsPresenterTests: XCTestCase {
+    func test_title_isLocalized() {
+        XCTAssertEqual(ImageCommentsPresenter.title, localized("IMAGE_COMMENTS_VIEW_TITLE"))
+    }
+    
+    // MARK: - Helpers
+    func localized(_ key: String, file: StaticString = #file, line: UInt = #line) -> String {
+        let table = "ImageComments"
+        let bundle = Bundle(for: ImageCommentsPresenter.self)
+        let value = bundle.localizedString(forKey: key, value: nil, table: table)
+        if value == key {
+            XCTFail("Missing localized string for key: \(key) in table: \(table)", file: file, line: line)
+        }
+        return value
+    }
+}

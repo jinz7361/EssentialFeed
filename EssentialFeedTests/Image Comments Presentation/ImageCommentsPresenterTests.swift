78334254ccfb80b9a741364e7b2428d5a5ca1c11
jinz7361 <jinz7361@gmail.com>
2024-07-19 16:36:25 -0400

    Inject calendar to make tests reliable

--- EssentialFeedTests/Image Comments Presentation/ImageCommentsPresenterTests.swift
+++ EssentialFeedTests/Image Comments Presentation/ImageCommentsPresenterTests.swift
@@ -13,7 +13,7 @@ class ImageCommentsPresenterTests: XCTestCase {
         XCTAssertEqual(ImageCommentsPresenter.title, localized("IMAGE_COMMENTS_VIEW_TITLE"))
     }
     
-    func text_map_createsViewModels() {
+    func test_map_createsViewModels() {
         let now = Date()
         let calendar = Calendar(identifier: .gregorian)
         let locale = Locale(identifier: "en_US_POSIX")
@@ -22,12 +22,12 @@ class ImageCommentsPresenterTests: XCTestCase {
             ImageComment(
                 id: UUID(),
                 message: "a message",
-                createdAt: now.adding(minutes: -5),
+                createdAt: now.adding(minutes: -5, calendar: calendar),
                 username: "a username"),
             ImageComment(
                 id: UUID(),
                 message: "another message",
-                createdAt: now.adding(days: -1),
+                createdAt: now.adding(days: -1, calendar: calendar),
                 username: "another username")
         ]
         

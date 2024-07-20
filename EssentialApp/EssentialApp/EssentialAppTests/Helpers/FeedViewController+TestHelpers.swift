1b6c068b2a2e725f4fc1266b877dbe4a0f2fe824
jinz7361 <jinz7361@gmail.com>
2024-07-20 17:35:22 -0400

    Rename FeedViewController to ListViewController since it can now render any collection of cell controllers

--- EssentialApp/EssentialApp/EssentialAppTests/Helpers/FeedViewController+TestHelpers.swift
+++ EssentialApp/EssentialApp/EssentialAppTests/Helpers/FeedViewController+TestHelpers.swift
@@ -8,7 +8,7 @@
 import UIKit
 import EssentialFeediOS
 
-extension FeedViewController {
+extension ListViewController {
     func simulateUserInitiatedFeedReload() {
         refreshControl?.simulatePullToRefresh()
     }
@@ -73,7 +73,7 @@ extension FeedViewController {
     }
 }
 
-extension FeedViewController {
+extension ListViewController {
     func simulateAppearance() {
         if !isViewLoaded {
             loadViewIfNeeded()

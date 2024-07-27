b07ae7a7e302251728471d87d5be88dea7316cb5
jinz7361 <jinz7361@gmail.com>
2024-07-27 00:10:10 -0400

    Render comments

--- EssentialFeediOS/Image Comments UI/Views/ImageCommentCell.swift
+++ EssentialFeediOS/Image Comments UI/Views/ImageCommentCell.swift
@@ -7,10 +7,8 @@
 
 import UIKit
 
-class ImageCommentCell: UITableViewCell {
+public class ImageCommentCell: UITableViewCell {
     @IBOutlet private(set) public var messageLabel: UILabel!
     @IBOutlet private(set) public var dateLabel: UILabel!
     @IBOutlet private(set) public var usernameLabel: UILabel!
-    
-    
 }
